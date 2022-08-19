5407fbf897510821a5632ea7c8c2f4c10e43c92b
JuanAngelJi <105176243+JuanAngelJi@users.noreply.github.com>
2022-08-19 16:45:05 +0200

    calculo

---
+++ index.js
@@ -0,0 +1,34 @@
+prompt("Ingresar numero 1");
+prompt("Ingresar numero 2");
+
+const calcular = document.querySelectorAll(".valornumerico");
+const operador = document.querySelectorAll(".operacion");
+
+function calculo(calcular) {
+  sumar((num1 = Number), (num2 = Number));
+  return num1 + num2;
+
+  console.log(calculo);
+}
+function calculo(calcular) {
+  restar((num1 = Number), (num2 = Number));
+  return num1 - num2;
+}
+console.log(calculo);
+
+function calculo(calcular) {
+  multiplicar((num1 = Number), (num2 = Number));
+  return num1 * num2;
+}
+
+console.log(calculo);
+
+function calculo(calcular) {
+  dividir((num1 = Number), (num2 = Number));
+  return num1 / num2;
+}
+console.log(calculo);
+
+const resulto = (calculo) => {
+  const operaciones = Array[(num1, num2)];
+};
